788a7f5fe019762f0dd960873f2afd596340bd7d
Simone Sanfratello <simone.sanfra@gmail.com>
2024-01-16 16:17:09 +0100

    wip

--- test/entities/on-subgraphs-2.test.js
+++ test/entities/on-subgraphs-2.test.js
@@ -5,7 +5,6 @@ const { test } = require('node:test')
 
 const { createComposerService, createGraphqlServices, graphqlRequest } = require('../helper')
 const { compose } = require('../../lib')
-const { default: pino } = require('pino')
 
 const booksSubgraph = () => {
   const schema = `
@@ -365,7 +364,7 @@ async function setupComposer (t) {
   ])
 
   const options = {
-    logger: pino({ level: 'debug' }),
+    // logger: pino({ level: 'debug', sync: true }),
     subgraphs: services.map(service => ({
       name: service.name,
       server: { host: service.host },
--- test/entities/on-subgraphs-3.test.js
+++ test/entities/on-subgraphs-3.test.js
@@ -67,94 +67,6 @@ function artistsSubgraph () {
 
   return { schema, reset, resolvers, entities, data }
 }
-
-function moviesSubgraph () {
-  const schema = `
-    type Movie {
-      id: ID!
-      title: String
-      directorId: ID
-      director: Artist
-    }
-  
-    type Query {
-      movies(ids: [ID!]!): [Movie]
-    }
-  
-    type Artist {
-      id: ID
-      movies: [Movie]
-    }
-  `
-
-  const data = {
-    movies: null
-  }
-
-  function reset () {
-    data.movies = {
-      10: {
-        id: 10,
-        title: 'Interstellar',
-        directorId: 101
-      },
-      11: {
-        id: 11,
-        title: 'Oppenheimer',
-        directorId: 101
-      },
-      12: {
-        id: 12,
-        title: 'La vita é bella',
-        directorId: 102
-      }
-    }
-  }
-
-  reset()
-
-  const resolvers = {
-    Query: {
-      async movies (_, { ids }) {
-        return Object.values(data.movies).filter(m => ids.includes(String(m.id)))
-      },
-    },
-    Movie: {
-      director: (parent, args, context, info) => {
-        return parent?.directorId ? { id: parent.directorId } : null
-      }
-    },
-    Artist: {
-      movies: (parent, args, context, info) => {
-        return Object.values(data.movies).filter(a => String(a.directorId) === String(parent.id))
-      }
-    }
-  }
-
-  const entities = {
-    Movie: {
-      resolver: { name: 'movies' },
-      pkey: 'id',
-      fkeys: [
-        {
-          type: 'Artist',
-          field: 'directorId',
-          pkey: 'id',
-          subgraph: 'artists-subgraph',
-          resolver: {
-            name: 'artists',
-            partialResults: (partialResults) => {
-              return partialResults.map(r => ({ id: r.directorId }))
-            }
-          }
-        }
-      ]
-    }
-  }
-
-  return { schema, reset, resolvers, entities, data }
-}
-
 function songsSubgraphs () {
   const schema = `
     type Song {
@@ -164,14 +76,15 @@ function songsSubgraphs () {
       singer: Artist
     }
   
-    type Query {
-      songs(ids: [ID!]!): [Song]
-    }
-  
     type Artist {
       id: ID
       songs: [Song]
     }
+
+    type Query {
+      songs(ids: [ID!]!): [Song]
+      artistsSongs(artistIds: [ID]!): [Artist]!
+    }  
   `
 
   const data = {
@@ -202,8 +115,24 @@ function songsSubgraphs () {
 
   const resolvers = {
     Query: {
-      async songs (_, { ids }) {
+      songs (_, { ids }) {
         return Object.values(data.songs).filter(s => ids.includes(String(s.id)))
+      },
+      artistsSongs (_, { artistIds }) {
+        return Object.values(Object.values(data.songs)
+          .reduce((artists, song) => {
+            if (!artistIds.includes(String(song.singerId))) { return artists }
+            if (artists[song.singerId]) {
+              artists[song.singerId].songs.push(song)
+            } else {
+              artists[song.singerId] = {
+                id: song.singerId,
+                songs: [song]
+              }
+            }
+            return artists
+          }, {})
+        )
       }
     },
     Song: {
@@ -236,13 +165,135 @@ function songsSubgraphs () {
           }
         }
       ]
+    },
+    Artist: {
+      pkey: 'id',
+      resolver: {
+        name: 'artistsSongs',
+        argsAdapter: (partialResults) => {
+          return { artistIds: partialResults.map(r => r?.id) }
+        }
+      }
     }
   }
 
   return { schema, reset, resolvers, entities, data }
 }
 
-test('entities on subgraph, scenario #3: entities with 1-1, 1-2-m, m-2-m relations solved on subgraphs', { only: 1 }, async (t) => {
+function moviesSubgraph () {
+  const schema = `
+    type Movie {
+      id: ID!
+      title: String
+      directorId: ID
+      director: Artist
+    }
+  
+    type Artist {
+      id: ID
+      movies: [Movie]
+    }
+
+    type Query {
+      movies(ids: [ID!]!): [Movie]
+      artistsMovies(artistIds: [ID]!): [Artist]!
+    }  
+  `
+
+  const data = {
+    movies: null
+  }
+
+  function reset () {
+    data.movies = {
+      10: {
+        id: 10,
+        title: 'Interstellar',
+        directorId: 101
+      },
+      11: {
+        id: 11,
+        title: 'Oppenheimer',
+        directorId: 101
+      },
+      12: {
+        id: 12,
+        title: 'La vita é bella',
+        directorId: 102
+      }
+    }
+  }
+
+  reset()
+
+  const resolvers = {
+    Query: {
+      movies (_, { ids }) {
+        return Object.values(data.movies).filter(m => ids.includes(String(m.id)))
+      },
+      artistsMovies (_, { artistIds }) {
+        return Object.values(Object.values(data.movies)
+          .reduce((artists, movie) => {
+            if (!artistIds.includes(String(movie.directorId))) { return artists }
+            if (artists[movie.directorId]) {
+              artists[movie.directorId].movies.push(movie)
+            } else {
+              artists[movie.directorId] = {
+                id: movie.directorId,
+                movies: [movie]
+              }
+            }
+            return artists
+          }, {})
+        )
+      }
+    },
+    Movie: {
+      director: (parent, args, context, info) => {
+        return parent?.directorId ? { id: parent.directorId } : null
+      }
+    },
+    Artist: {
+      movies: (parent, args, context, info) => {
+        return Object.values(data.movies).filter(a => String(a.directorId) === String(parent.id))
+      }
+    }
+  }
+
+  const entities = {
+    Movie: {
+      resolver: { name: 'movies' },
+      pkey: 'id',
+      fkeys: [
+        {
+          type: 'Artist',
+          field: 'directorId',
+          pkey: 'id',
+          subgraph: 'artists-subgraph',
+          resolver: {
+            name: 'artists',
+            partialResults: (partialResults) => {
+              return partialResults.map(r => ({ id: r.directorId }))
+            }
+          }
+        }
+      ]
+    },
+    Artist: {
+      pkey: 'id',
+      resolver: {
+        name: 'artistsMovies',
+        argsAdapter: (partialResults) => {
+          return { artistIds: partialResults.map(r => r?.id) }
+        }
+      }
+    }
+  }
+
+  return { schema, reset, resolvers, entities, data }
+}
+
+test('entities on subgraph, scenario #3: entities with 1-1, 1-2-m, m-2-m relations solved on subgraphs', async (t) => {
   let service
 
   t.before(async () => {
@@ -281,7 +332,7 @@ test('entities on subgraph, scenario #3: entities with 1-1, 1-2-m, m-2-m relatio
     ])
 
     const options = {
-      logger: pino({ level: 'debug' }),
+      logger: pino({ level: 'debug', sync: true }),
       defaultArgsAdapter: (partialResults) => {
         return { ids: partialResults.map(r => r?.id) }
       },
@@ -306,52 +357,52 @@ test('entities on subgraph, scenario #3: entities with 1-1, 1-2-m, m-2-m relatio
     //       { title: 'The bitter end', singer: { firstName: 'Brian', lastName: 'Molko', profession: 'Singer' } },
     //       { title: 'Vieni via con me', singer: { firstName: 'Roberto', lastName: 'Benigni', profession: 'Director' } }]
     //   }
-    // }
+    // },
 
-    {
-      name: 'should run a query that resolve entities with a 1-to-many relation',
-      query: '{ artists (ids: ["103","102"]) { lastName, songs { title } } }',
-      result: {
-        artists: [
-          { lastName: 'Benigni', songs: [{ title: 'Vieni via con me' }] },
-          { lastName: 'Molko', songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] }]
-      }
-    }
+    // {
+    //   name: 'should run a query that resolve entities with a 1-to-many relation',
+    //   query: '{ artists (ids: ["103","102"]) { lastName, songs { title } } }',
+    //   result: {
+    //     artists: [
+    //       { lastName: 'Benigni', songs: [{ title: 'Vieni via con me' }] },
+    //       { lastName: 'Molko', songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] }]
+    //   }
+    // },
 
     // {
     //   name: 'should run a query that resolve multiple entities on different subgrapgh on the same node',
     //   query: '{ artists (ids: ["103","101","102"]) { lastName, songs { title }, movies { title } } }',
     //   result: {
     //     artists: [
-    //       { lastName: 'Nolan', songs: [], movies: [{ title: 'Interstellar' }, { title: 'Oppenheimer' }] },
+    //       { lastName: 'Nolan', songs: null, movies: [{ title: 'Interstellar' }, { title: 'Oppenheimer' }] },
     //       { lastName: 'Benigni', songs: [{ title: 'Vieni via con me' }], movies: [{ title: 'La vita é bella' }] },
-    //       { lastName: 'Molko', songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }], movies: [] }
+    //       { lastName: 'Molko', songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }], movies: null }
     //     ]
     //   }
     // },
 
-    // {
-    //   name: 'should run a query with double nested results',
-    //   query: '{ artists (ids: ["103"]) { songs { title, singer { firstName, lastName } } } }',
-    //   result: { artists: [{ songs: [{ title: 'Every you every me', singer: { firstName: 'Brian', lastName: 'Molko' } }, { title: 'The bitter end', singer: { firstName: 'Brian', lastName: 'Molko' } }] }] }
-    // },
+    {
+      name: 'should run a query with double nested results',
+      query: '{ artists (ids: ["103"]) { songs { title } } }', // , singer { firstName, lastName }
+      result: { artists: [{ songs: [{ title: 'Every you every me', singer: { firstName: 'Brian', lastName: 'Molko' } }, { title: 'The bitter end', singer: { firstName: 'Brian', lastName: 'Molko' } }] }] }
+    },
 
-  // {
-  //   name: 'should run a query with insane nested results',
-  //   query: '{ artists (ids: ["103"]) { songs { singer { songs { singer { songs { title } }} } } } }',
-  //   result: { artists: [{ songs: [{ singer: { songs: [{ singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }, { singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }] } }, { singer: { songs: [{ singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }, { singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }] } }] }] }
-  // }
+    // {
+    //   name: 'should run a query with insane nested results',
+    //   query: '{ artists (ids: ["103"]) { songs { singer { songs { singer { songs { title } }} } } } }',
+    //   result: { artists: [{ songs: [{ singer: { songs: [{ singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }, { singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }] } }, { singer: { songs: [{ singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }, { singer: { songs: [{ title: 'Every you every me' }, { title: 'The bitter end' }] } }] } }] }] }
+    // }
   ]
 
   for (const c of requests) {
-    await t.test(c.name, { only: 1 }, async (t) => {
-      // console.log(' *************')
-      // console.log(c.query)
-      // console.log(' *************')
+    await t.test(c.name, async (t) => {
+      console.log(' *************')
+      console.log(c.query)
+      console.log(' *************')
 
       const result = await graphqlRequest(service, c.query, c.variables)
 
-      // console.log(JSON.stringify(result, null, 2))
+      console.log(JSON.stringify(result, null, 2))
 
       assert.deepStrictEqual(result, c.result)
     })
