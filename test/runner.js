8a6df2cf02591aeea7e4152dec9272a14823f062
Simone Sanfratello <simone.sanfra@gmail.com>
2024-01-25 17:56:19 +0100

    wip

--- test/runner.js
+++ test/runner.js
@@ -16,8 +16,33 @@ const stream = run({
   only: !!process.env.ONLY
 })
 
-stream.on('test:fail', () => {
+const fails = []
+
+stream.on('test:fail', (t) => {
+  if (!t.details.error.cause) { return }
+  fails.push(t)
   process.exitCode = 1
+})
+
+stream.on('close', () => {
+  // TODO better formatting
+  if (fails.length > 0) {
+    for (const f of fails) {
+      console.log('\n')
+      console.log('Test:', f.name)
+      console.log('Error:', f.details.error.cause)
+      console.log('---')
+      // {
+      //   name: 'should run a query with double nested results',
+      //   nesting: 1,
+      //   testNumber: 1,
+      //   details: { duration_ms: 332.996288, error: [Error] },
+      //   line: 398,
+      //   column: 13,
+      //   file: '/home/simone/code/graphql-composer/test/entities/on-subgraphs-3.test.js'
+      // }
+    }
+  }
 })
 
 stream.compose(reporter).pipe(process.stdout)
