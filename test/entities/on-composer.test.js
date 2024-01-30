20e42d838cf4124896ccffe899f77ea5ab5c71a3
Simone Sanfratello <simone.sanfra@gmail.com>
2024-01-30 15:43:28 +0100

    wip

--- test/entities/on-composer.test.js
+++ test/entities/on-composer.test.js
@@ -14,145 +14,141 @@ test('composer on top', async () => {
       return { where: { id: { in: partialResults.map(r => r.id) } } }
     },
     entities: {
-      'artists-subgraph': {
-        Artist: {
-          resolver: { name: 'artists' },
-          pkey: 'id',
-          many: [
-            {
-              type: 'Movie',
-              as: 'movies',
-              pkey: 'id',
-              fkey: 'directorId',
-              subgraph: 'movies-subgraph',
-              resolver: {
-                name: 'movies',
-                argsAdapter: (artistIds) => {
-                  return { where: { directorId: { in: artistIds } } }
-                },
-                partialResults: (artists) => {
-                  return artists.map(r => r.id)
-                }
-              }
-            },
-            {
-              type: 'Song',
-              as: 'songs',
-              pkey: 'id',
-              fkey: 'singerId',
-              subgraph: 'songs-subgraph',
-              resolver: {
-                name: 'songs',
-                argsAdapter: (artistIds) => {
-                  return { where: { singerId: { in: artistIds } } }
-                },
-                partialResults: (artists) => {
-                  return artists.map(r => r.id)
-                }
+      Artist: {
+        subgraph: 'artists-subgraph',
+        resolver: { name: 'artists' },
+        pkey: 'id',
+        many: [
+          {
+            type: 'Movie',
+            as: 'movies',
+            pkey: 'id',
+            fkey: 'directorId',
+            subgraph: 'movies-subgraph',
+            resolver: {
+              name: 'movies',
+              argsAdapter: (artistIds) => {
+                return { where: { directorId: { in: artistIds } } }
+              },
+              partialResults: (artists) => {
+                return artists.map(r => r.id)
               }
             }
-          ]
-        }
+          },
+          {
+            type: 'Song',
+            as: 'songs',
+            pkey: 'id',
+            fkey: 'singerId',
+            subgraph: 'songs-subgraph',
+            resolver: {
+              name: 'songs',
+              argsAdapter: (artistIds) => {
+                return { where: { singerId: { in: artistIds } } }
+              },
+              partialResults: (artists) => {
+                return artists.map(r => r.id)
+              }
+            }
+          }
+        ]
       },
-      'movies-subgraph': {
-        Movie: {
-          resolver: { name: 'movies' },
-          pkey: 'id',
-          fkeys: [
-            {
-              type: 'Artist',
-              as: 'director',
-              field: 'directorId',
-              pkey: 'id',
-              resolver: {
-                name: 'movies',
-                argsAdapter: (movieIds) => {
-                  return { where: { directorId: { in: movieIds.map(r => r.id) } } }
-                },
-                partialResults: (movies) => {
-                  return movies.map(r => ({ id: r.directorId }))
-                }
+      Movie: {
+        subgraph: 'movies-subgraph',
+        resolver: { name: 'movies' },
+        pkey: 'id',
+        fkeys: [
+          {
+            type: 'Artist',
+            as: 'director',
+            field: 'directorId',
+            pkey: 'id',
+            resolver: {
+              name: 'movies',
+              argsAdapter: (movieIds) => {
+                return { where: { directorId: { in: movieIds.map(r => r.id) } } }
+              },
+              partialResults: (movies) => {
+                return movies.map(r => ({ id: r.directorId }))
               }
             }
-          ],
-          many: [
-            {
-              type: 'Cinema',
-              as: 'cinemas',
-              pkey: 'id',
-              fkey: 'movieIds',
-              subgraph: 'cinemas-subgraph',
-              resolver: {
-                name: 'cinemas',
-                argsAdapter: (movieIds) => {
-                  return { where: { movieIds: { in: movieIds } } }
-                },
-                partialResults: (movies) => {
-                  return movies.map(r => r.id)
-                }
+          }
+        ],
+        many: [
+          {
+            type: 'Cinema',
+            as: 'cinemas',
+            pkey: 'id',
+            fkey: 'movieIds',
+            subgraph: 'cinemas-subgraph',
+            resolver: {
+              name: 'cinemas',
+              argsAdapter: (movieIds) => {
+                return { where: { movieIds: { in: movieIds } } }
+              },
+              partialResults: (movies) => {
+                return movies.map(r => r.id)
               }
             }
-          ]
-        }
+          }
+        ]
       },
-      'songs-subgraph': {
-        Song: {
-          resolver: { name: 'songs' },
-          pkey: 'id',
-          fkeys: [
-            {
-              type: 'Artist',
-              as: 'singer',
-              field: 'singerId',
-              pkey: 'id',
-              resolver: {
-                name: 'songs',
-                argsAdapter: (songIds) => {
-                  return { where: { singerId: { in: songIds.map(r => r.id) } } }
-                },
-                partialResults: (songs) => {
-                  return songs.map(r => ({ id: r.singerId }))
-                }
+      Song: {
+        subgraph: 'songs-subgraph',
+        resolver: { name: 'songs' },
+        pkey: 'id',
+        fkeys: [
+          {
+            type: 'Artist',
+            as: 'singer',
+            field: 'singerId',
+            pkey: 'id',
+            resolver: {
+              name: 'songs',
+              argsAdapter: (songIds) => {
+                return { where: { singerId: { in: songIds.map(r => r.id) } } }
+              },
+              partialResults: (songs) => {
+                return songs.map(r => ({ id: r.singerId }))
               }
             }
-          ]
-        }
+          }
+        ]
       },
-      'cinemas-subgraph': {
-        Cinema: {
-          resolver: { name: 'cinemas' },
-          pkey: 'id',
-          many: [
-            {
-              type: 'Movie',
-              as: 'movies',
-              pkey: 'movieIds',
-              fkey: 'id',
-              subgraph: 'movies-subgraph',
-              resolver: {
-                name: 'movies',
-                argsAdapter: (movieIds) => {
-                  return { where: { id: { in: movieIds } } }
-                },
-                partialResults: (movies) => {
-                  return Array.from(new Set([...movies.flat().map(m => m.movieIds).flat()]))
-                }
+      Cinema: {
+        subgraph: 'cinemas-subgraph',
+        resolver: { name: 'cinemas' },
+        pkey: 'id',
+        many: [
+          {
+            type: 'Movie',
+            as: 'movies',
+            pkey: 'movieIds',
+            fkey: 'id',
+            subgraph: 'movies-subgraph',
+            resolver: {
+              name: 'movies',
+              argsAdapter: (movieIds) => {
+                return { where: { id: { in: movieIds } } }
+              },
+              partialResults: (movies) => {
+                return Array.from(new Set([...movies.flat().map(m => m.movieIds).flat()]))
               }
             }
-          ]
-        }
+          }
+        ]
       }
     }
   }
 
-  await test('should generate entities for composer on top for multiple subgraphs', async t => {
+  await test('should generate entities for composer on top for multiple subgraphs', { skip: 1 }, async t => {
     const expectedSchema = 'type Artist { id: ID, movies: [Movie], songs: [Song] }\n\n' +
-      'type Movie { id: ID!, director: Artist, cinemas: [Cinema] }\n\n' +
-      'type Song { id: ID!, singer: Artist }\n\n' +
-      'type Cinema { id: ID!, movies: [Movie] }\n\n' +
-      'type Query {\n' +
-      '  _composer: String \n' +
-      '}'
+        'type Movie { id: ID!, director: Artist, cinemas: [Cinema] }\n\n' +
+        'type Song { id: ID!, singer: Artist }\n\n' +
+        'type Cinema { id: ID!, movies: [Movie] }\n\n' +
+        'type Query {\n' +
+        '  _composer: String \n' +
+        '}'
 
     const expectedResolvers = {
       Artist: {},
@@ -162,9 +158,10 @@ test('composer on top', async () => {
     }
 
     const expectedEntities = {
-      Artist: { subgraphName: 'artists-subgraph', pkey: 'id', fkeys: [] },
+      Artist: { subgraph: 'artists-subgraph', resolver: { name: 'artists' }, pkey: 'id', fkeys: [] },
       Movie: {
-        subgraphName: 'movies-subgraph',
+        subgraph: 'movies-subgraph',
+        resolver: { name: 'movies' },
         pkey: 'id',
         fkeys: [
           {
@@ -181,7 +178,8 @@ test('composer on top', async () => {
         ]
       },
       Song: {
-        subgraphName: 'songs-subgraph',
+        subgraph: 'songs-subgraph',
+        resolver: { name: 'songs' },
         pkey: 'id',
         fkeys: [
           {
@@ -198,7 +196,7 @@ test('composer on top', async () => {
         ]
       },
       Cinema: {
-        subgraphName: 'cinemas-subgraph', pkey: 'id', fkeys: []
+        subgraph: 'cinemas-subgraph', resolver: { name: 'cinemas' }, pkey: 'id', fkeys: []
       }
     }
 
@@ -244,7 +242,7 @@ test('composer on top', async () => {
     assertObject(entities, expectedEntities)
   })
 
-  await test('should generate entities resolvers for composer on top for multiple subgraphs', { skip: 1 }, async t => {
+  await test('should generate entities resolvers for composer on top for multiple subgraphs', { skip: 0 }, async t => {
     const services = await createGraphqlServices(t, [
       {
         name: 'artists-subgraph',
@@ -284,8 +282,8 @@ test('composer on top', async () => {
       {
         query: '{ movies (where: { id: { in: ["10","11","12"] } }) { title, director { lastName } } }',
         expected: { movies: [{ title: 'Interstellar', director: { lastName: 'Nolan' } }, { title: 'Oppenheimer', director: { lastName: 'Nolan' } }, { title: 'La vita é bella', director: { lastName: 'Benigni' } }] }
-      },
-
+      }
+      /*
       {
         query: '{ movies (where: { id: { in: ["10","11","12"] } }) { title, cinemas { name } } }',
         expected: { movies: [{ title: 'Interstellar', cinemas: [{ name: 'Odeon' }, { name: 'Main Theatre' }] }, { title: 'Oppenheimer', cinemas: [] }, { title: 'La vita é bella', cinemas: [{ name: 'Odeon' }, { name: 'Main Theatre' }] }] }
@@ -407,15 +405,16 @@ test('composer on top', async () => {
           }]
         }
       }
+      */
     ]
 
     for (const request of requests) {
       const response = await graphqlRequest(service, request.query, request.variables)
 
       assert.deepStrictEqual(response, request.expected, 'should get expected result from composer service,' +
-        '\nquery: ' + request.query +
-        '\nexpected' + JSON.stringify(request.expected, null, 2) +
-        '\nresponse' + JSON.stringify(response, null, 2)
+          '\nquery: ' + request.query +
+          '\nexpected' + JSON.stringify(request.expected, null, 2) +
+          '\nresponse' + JSON.stringify(response, null, 2)
       )
     }
   })
